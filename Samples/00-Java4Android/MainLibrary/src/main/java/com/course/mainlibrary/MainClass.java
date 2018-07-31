f115530c6ee7c39a58480c4f10452922c1daf99d
Adhamkh <adhamkhwaldeh@gmail.com>
2018-07-31 15:29:00 +0300

    Second Commit
    Second Commit

--- Samples/00-Java4Android/MainLibrary/src/main/java/com/course/mainlibrary/MainClass.java
+++ Samples/00-Java4Android/MainLibrary/src/main/java/com/course/mainlibrary/MainClass.java
@@ -1,6 +1,8 @@
 package com.course.mainlibrary;
 
+import java.util.List;
 import java.util.Scanner;
+import java.util.Vector;
 
 public class MainClass {
 
@@ -11,8 +13,8 @@ public class MainClass {
         //Variables
         /*
         int age = 30;
-        float weight = 85.5f;
         long idNumber = 12010016485L;
+        float weight = 85.5f;
 
         String firstName = "Mohammad";
         String lastName = "Mohammad";
@@ -63,9 +65,9 @@ public class MainClass {
 
 
         //Relational operation
-        int c = 70;
+        /*int c = 70;
         int d = 80;
-        d = sc.nextInt();
+//        d = sc.nextInt();
         if (c > d) {
             System.out.print("C bigger than d");
         } else if (c == d) {
@@ -73,12 +75,12 @@ public class MainClass {
         } else {
             System.out.print("d bigger than c");
         }
-        System.out.println();
+        System.out.println();*/
 
 
         //Loops
         //for loop
-        for (int i = 0; i < 10; i++) {
+       /* for (int i = 0; i < 10; i++) {
             System.out.print("Line number " + String.valueOf(i));
             System.out.println();
         }
@@ -88,12 +90,37 @@ public class MainClass {
             System.out.println();
         }
 
+        //while loop
+        int cnt = 0;
+        while (cnt < 10) {
+            System.out.print("next step " + String.valueOf(cnt));
+            cnt++;
+        }*/
 
+
+        //Array started
+
+//        int[] mNumber = new int[10];
+//        for (int i = 0; i < 10; i++)
+//            mNumber[i] = i;
+
+
+        //List started
+
+//        List<String> stringList = new Vector<>();
+//        for (int i = 0; i < 10; i++) {
+//            stringList.add(String.valueOf(i));
+//        }
+
+        //Function started
+
+        int speed = CarSpeed(100, 10);
+        System.out.print(speed);
 
     }
 
 
-    int CarSpeed(int distance, int time) {
+    static int CarSpeed(int distance, int time) {
         return distance / time;
     }
 
